9098eb94eb2b644d4993334269166ce40fc064c2
Objectivitix <79152594+Objectivitix@users.noreply.github.com>
2025-01-05 21:35:22 -0500

    Restore Dominic functionality, and also:
    - Unlimit synthesis length and skip invalid chars instead of throwing
    - No more looped/double-passed characters
    - Remove background rectangle
    - Robust TypeScript frontend

---
+++ client/src/types.d.ts
@@ -0,0 +1,4 @@
+declare module "./blot/src/*";
+
+declare type Polyline = [number, number][];
+declare type Polylines = Polyline[];
--- server/main.py
+++ server/main.py
@@ -1,47 +1,74 @@
 import os
+from typing import List
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
+from pydantic import BaseModel
+from snowflake import SnowflakeGenerator
 
 from handsynth.demo import Hand
 
+OUTPUT_DIR = "img"
+
 app = FastAPI()
 
-IMG_DIR = "img"
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Frontend URLs here
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
-if not os.path.exists(IMG_DIR):
-    os.makedirs(IMG_DIR)
+hand = Hand(max_line_length=100)
+
+snow_gen = SnowflakeGenerator(0)
 
 
-def image():
-    print("Generating image...")
-    lines = [
-        "Now this is a story all about how",
-        "My life got flipped turned upside down",
-        "And I'd like to take a minute, just sit right there",
-        "I'll tell you how I became the prince of a town called Bel-Air",
-    ]
-    biases = [.75 for _ in lines]
-    styles = [9 for _ in lines]
-    stroke_colors = ['red', 'green', 'black', 'blue']
-    stroke_widths = [1, 2, 1, 2]
+class SynthesisInput(BaseModel):
+    text: str
 
-    hand = Hand()
-    filename = 'img/usage_demo.svg'
+
+def gen_svg_image(lines: List[str], *, bias: float = 0.5, style: int = 0, stroke_color: str = "black", stroke_width: float = 1):
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+        print(f"Created ${OUTPUT_DIR} directory.")
+
+    print("Generating SVG image of handwriting...")
+
+    lines_n = len(lines)
+    output_file_path = f"img/output_{next(snow_gen)}.svg"
+
     hand.write(
-        filename=filename,
+        filename=output_file_path,
         lines=lines,
-        biases=biases,
-        styles=styles,
-        stroke_colors=stroke_colors,
-        stroke_widths=stroke_widths
+        biases=[bias for _ in range(lines_n)],
+        styles=[style for _ in range(lines_n)],
+        stroke_colors=[stroke_color for _ in range(lines_n)],
+        stroke_widths=[stroke_width for _ in range(lines_n)],
     )
-    return filename
+
+    return output_file_path
 
 
-@app.get("/generate-handwriting")
-def generate_handwriting():
-    filename = image()
-    return FileResponse(filename, media_type='image/svg+xml', headers={"Content-Disposition": f"attachment; filename={os.path.basename(filename)}"})
+# @app.get("/generate-handwriting")
+# def generate_handwriting():
+#     filename = image()
+#     return FileResponse(filename, media_type='image/svg+xml', headers={"Content-Disposition": f"attachment; filename={os.path.basename(filename)}"})
+
+
+@app.get("/")
+def index():
+    return {"yay": "Things are looking up!"}
+
+
+@app.post("/synthesize")
+def synthesize(inp: SynthesisInput):
+    with open(gen_svg_image([inp.text], bias=1), "r", encoding="utf-8") as f:
+        svg_content = f.read()
+
+    return {"svg": svg_content}
 
 
 if __name__ == "__main__":
